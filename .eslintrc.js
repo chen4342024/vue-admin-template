09e7b1b3813f8d7ba2d934c7958378927e0409c8
chen_jia_jia <chenjiajia@zuzuche.com>
2018-12-19 19:50:51 +0800

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,15 +9,12 @@ module.exports = {
         node: true,
         es6: true,
     },
-    extends: ['plugin:vue/essential', '@vue/prettier'],
+    extends: ['plugin:vue/essential', '@vue/prettier', "plugin:prettier/recommended"],
 
     // add your custom rules here
     //it is base on https://github.com/vuejs/eslint-config-vue
     rules: {
         "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
         "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-        'indent': [2, 4, {
-            'SwitchCase': 1
-        }],
     }
 }
